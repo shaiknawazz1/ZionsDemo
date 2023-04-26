73c0e6b9e2f626b6beb8b221398ded4883dc3b26
shaiknawazz1 <94003983+shaiknawazz1@users.noreply.github.com>
2023-04-26 16:57:36 +0530

    added demo.py and sample test data

---
+++ rfp-zions/app/src/pyspark/demo.py
@@ -0,0 +1,29 @@
+# Code converted on 2023-04-24 13:21:55
+import os
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark import SparkContext
+from pyspark.sql.session import SparkSession
+import pandas
+sc = SparkContext('local')
+spark = SparkSession(sc)
+
+try:
+
+    # Processing node DSLink2, type SOURCE
+    # COLUMN COUNT: 7
+    # Original node name Get_Employee_Data, link DSLink2
+
+    df = spark.read.csv("../../test-data/demo/input.csv", header=True)
+
+    df2 = df.select("CustomerNumber", "GivenName", "MiddleName", "LastName", "FamilyName", concat_ws(" ", df.GivenName, df.MiddleName, df.LastName)
+                    .alias("FullName"), "NameprefixTxt")
+    # df2.show()
+    csv_file_path = "../../test-data/demo/pySpark_output.csv"
+    df2.toPandas().to_csv(csv_file_path)
+
+except OSError:
+    print('Error Occurred')
+
+
+quit()
